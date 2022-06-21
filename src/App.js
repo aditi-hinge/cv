8708d49b547f913e2e4a8d82c7874db2ba92aa59
aditi-hinge <aditi.hinge@gmail.com>
2022-06-21 21:19:47 +0900

    App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Resume from "./components/Resume";
+import styled from "styled-components";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Resume />
+    </Container>
   );
 }
 
-export default App;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
